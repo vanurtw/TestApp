8651a776deb60249a0d06f47dc589ae006bc36b9
vano <vanuartw@mail.ru>
2025-01-15 00:06:02 +0300

    Создание view/serializer для лотов

--- test_app/app/serializers.py
+++ test_app/app/serializers.py
@@ -17,13 +17,14 @@ class CustomerSerializer(serializers.ModelSerializer):
     customer_code_main = serializers.SerializerMethodField()
 
     def get_customer_code_main(self, obj, *args, **kwargs):
-        customer_code_main = self.initial_data.get('customer_code_main', None)
-        if customer_code_main:
-            try:
-                obj.customer_code_main_id = customer_code_main
-                obj.save()
-            except Exception as ex:
-                raise ValidationError(str(ex))
+        if self.context.get('action') == 'partial_update':
+            customer_code_main = self.initial_data.get('customer_code_main', None)
+            if customer_code_main:
+                try:
+                    obj.customer_code_main_id = customer_code_main
+                    obj.save()
+                except Exception as ex:
+                    raise ValidationError(str(ex))
         if obj.customer_code_main:
             serializer = CustomerReviewSerializer(obj.customer_code_main)
             return serializer.data
@@ -49,13 +50,14 @@ class LotSerializer(serializers.ModelSerializer):
     customer_code = serializers.SerializerMethodField()
 
     def get_customer_code(self, obj, *args, **kwargs):
-        customer_code = self.initial_data.get('customer_code', None)
-        if customer_code:
-            try:
-                obj.customer_code_id = customer_code
-                obj.save()
-            except Exception as ex:
-                raise ValidationError(str(ex))
+        if self.context.get('action') == 'partial_update':
+            customer_code = self.initial_data.get('customer_code', None)
+            if customer_code:
+                try:
+                    obj.customer_code_id = customer_code
+                    obj.save()
+                except Exception as ex:
+                    raise ValidationError(str(ex))
 
         if obj.customer_code:
             serializer = CustomerReviewSerializer(obj.customer_code)
