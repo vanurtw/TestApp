19cde85f5a9431dcb1cf01134091674dab5afdec
vano <vanuartw@mail.ru>
2025-01-15 11:28:12 +0300

    Настройка swagger

--- test_app/app/views.py
+++ test_app/app/views.py
@@ -5,6 +5,17 @@ from .models import Customer, Lot
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+ValidateSchema = openapi.Schema(
+    type=openapi.TYPE_OBJECT,
+    properties={'error': openapi.Schema(description='detail', type=openapi.TYPE_STRING)}
+)
+
+ErrorObject = openapi.Schema(
+    type=openapi.TYPE_STRING,
+)
 
 
 class CustomerAPIViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
@@ -24,10 +35,26 @@ class CustomerAPIViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
     def get_queryset(self):
         return Customer.objects.all()
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('customer_inn', openapi.IN_QUERY, type=openapi.TYPE_STRING, description='ИНН'),
+            openapi.Parameter('is_organization', openapi.IN_QUERY, type=openapi.TYPE_BOOLEAN, description='Юр. лицо'),
+            openapi.Parameter('is_person', openapi.IN_QUERY, type=openapi.TYPE_BOOLEAN, description='Физ. лицо'),
+            openapi.Parameter('search', openapi.IN_QUERY, type=openapi.TYPE_STRING,
+                              description='Поиск по наименованию и коду контрагента'),
+            openapi.Parameter('ordering', openapi.IN_QUERY, type=openapi.TYPE_STRING,
+                              description='Сортировка по наименованию и Юр. адрес: customer_name/customer_legal_address'),
+        ]
+    )
     def list(self, request, *args, **kwargs):
         '''Чтение всех записей контрагентов'''
         return super().list(request, *args, **kwargs)
 
+    @swagger_auto_schema(
+        responses={
+            404: openapi.Response(description='Объект не найден', schema=ErrorObject)
+        }
+    )
     def retrieve(self, request, *args, **kwargs):
         '''Чтение конкретной записи контрагентов по id'''
         return super().retrieve(request, *args, **kwargs)
@@ -35,6 +62,12 @@ class CustomerAPIViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
     def perform_update(self, serializer):
         serializer.save()
 
+    @swagger_auto_schema(
+        responses={
+            404: openapi.Response(description='Объект не найден'),
+            400: openapi.Response(description='Ошибка валидации', schema=ValidateSchema)
+        }
+    )
     def partial_update(self, request, *args, **kwargs):
         '''Частичное обновление записи контрагентов'''
         instance = self.get_object()
@@ -61,10 +94,23 @@ class LotAPIViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
         context['action'] = self.action
         return context
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('search', openapi.IN_QUERY, type=openapi.TYPE_STRING,
+                              description='Поиск по наименованию контрагента и дате доставки'),
+            openapi.Parameter('ordering', openapi.IN_QUERY, type=openapi.TYPE_STRING,
+                              description='Сортировка по цене и дате доставки: price/date_delivery'),
+        ]
+    )
     def list(self, request, *args, **kwargs):
         '''Чтение всех записей лотов'''
         return super().list(request, *args, **kwargs)
 
+    @swagger_auto_schema(
+        responses={
+            404: openapi.Response(description='Объект не найден', schema=ErrorObject)
+        }
+    )
     def retrieve(self, request, *args, **kwargs):
         '''Чтение конкретной записи лотов по id'''
         return super().retrieve(request, *args, **kwargs)
@@ -72,6 +118,12 @@ class LotAPIViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
     def perform_update(self, serializer):
         serializer.save()
 
+    @swagger_auto_schema(
+        responses={
+            404: openapi.Response(description='Объект не найден'),
+            400: openapi.Response(description='Ошибка валидации', schema=ValidateSchema)
+        }
+    )
     def partial_update(self, request, *args, **kwargs):
         '''Частичное обновление записи лота'''
         instance = self.get_object()
