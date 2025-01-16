cc95d57eaff3c1d176c82ca8b3c9f02566add8b4
vano <vanuartw@mail.ru>
2025-01-16 10:16:55 +0300

    Подключение postgres

--- test_app/test_app/settings.py
+++ test_app/test_app/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'test_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'customers-lots',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -123,3 +127,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+
+
